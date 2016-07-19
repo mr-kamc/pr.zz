f3f2f4c003802681c71ce705b66c592b194ac1f5
Алексей <mr.kamc@gmail.com>
2016-07-19 13:32:47 +0300

    структора проекта

---
+++ controllers/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace controllers;
+
+
+class Index
+{
+
+    public function __construct()
+    {
+        echo 'мы в контроллере index!';
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/libs/autoload.php';
+
+
+//$url = $_GET['url'];
+//echo $url;
+
+$controller = new \controllers\Index();
